c91098deb73d20c8a93cbfafde7b83f12bfe553a
Zoya Paliashchuk <zoyapaliashchuk@gmail.com>
2022-03-07 00:04:25 +0100

    я пыталась

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,12 @@
-function factorial(n) {
-  // ваш код...
+fuction factorial (n) {
+  let n! = n * (n - 1) * (n - 2) * ...*1
+  let result  = n!
+      for (let n!; n! > 0; n!++) {
+        return result; 
+      }
+ if ((n! > 0) && (n! % 2 == 0) & (n! % 2! == 0 ) && (++n! % 2 == 0) ) {
+   alert (result);
+ } if (n! == 0 && n! == 1) {
+     result (1); 
+   }
 }
